96623ddbc77823d8e45eed739c4e4900cb7ef659
pandabear-de <public@cameloteast.de>
2017-07-29 03:04:51 +0200

    small visual adjustments (added spaces etc)

--- diktat.py
+++ diktat.py
@@ -1,10 +1,8 @@
 import logging
 import csv
 import random
-from datetime import datetime, timedelta, date
+from datetime import datetime
 import configparser
-
-
 config = configparser.ConfigParser()
 config.read('diktat.ini')
 
@@ -13,8 +11,9 @@ file_path = config['Paths']['file_path']
 file_wordlist_name = config['Paths']['file_wordlist_name']
 file_wordlistOUT_name = config['Paths']['file_wordlistOUT_name']
 
+
 class Word:
-    def __init__(self,user,insert_date,word,last_test_date,number_tests,consecutive_correct):
+    def __init__(self, user, insert_date, word, last_test_date, number_tests, consecutive_correct):
         self.user = user
         if insert_date == "":
             self.insert_date = datetime.now().date()
@@ -28,7 +27,7 @@ class Word:
         self.number_tests = number_tests
         self.consecutive_correct = consecutive_correct
 
-    #def add_word(self,user,word):
+    # def add_word(self,user,word):
     #    logger.debug("### add_word ###")
     #    self.user = user
     #    self.insert_date = datetime.now().strftime('%Y%m%d-%H%M%S')  # yyyymmdd
@@ -37,14 +36,15 @@ class Word:
     #    self.number_tests = 0
     #    self.consecutive_correct = 0
 
-    def test_word(self,test_result):
+    def test_word(self, test_result):
         logger.debug("### test_word ###")
         self.last_test_date = datetime.now().date()
         self.number_tests = self.number_tests + 1
         if test_result:
-            self.consecutive_correct = self.consecutive_correct +1
+            self.consecutive_correct = self.consecutive_correct + 1
         elif not test_result:
             self.consecutive_correct = 0
+
 
 def read_wordlist():
     """read_wordlist reads __read_wordlist__ into memory"""
@@ -57,9 +57,9 @@ def read_wordlist():
     data = []
     for row in reader:
         user = row[0]
-        insert_date = row[1] #YYYYMMDD
+        insert_date = row[1]  # YYYYMMDD
         word = row[2]
-        last_test_date = row[3] # YYYYMMDD
+        last_test_date = row[3]  # YYYYMMDD
         number_tests = int(row[4])
         consecutive_correct = int(row[5])
         data.append(Word(user, insert_date, word, last_test_date, number_tests, consecutive_correct))
@@ -70,18 +70,20 @@ def read_wordlist():
     file.close()
     return data
 
+
 def write_wordlist(wordlist):
     """Saves CSV to drive"""
     logger.debug("### write_wordlist ###")
     with open(file_path + file_wordlistOUT_name, "w") as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
-        writer.writerow(("User","Insert_Date","Word","Last_Test_Date","Number_Tests","Consecutive_Correct"))
-        for i in range(0,len(wordlist)):
+        writer.writerow(("User", "Insert_Date", "Word", "Last_Test_Date", "Number_Tests", "Consecutive_Correct"))
+        for i in range(0, len(wordlist)):
             writer.writerow((wordlist[i].user, wordlist[i].insert_date.strftime('%Y%m%d'), wordlist[i].word, wordlist[i].last_test_date.strftime('%Y%m%d'), str(wordlist[i].number_tests), str(wordlist[i].consecutive_correct)))
 
         csv_file.close()
 
-def add_word(wordlist,user,word):
+
+def add_word(wordlist, user, word):
     logger.debug("### add_word ###")
     logger.debug("added User: " + user + " Word: " + word)
     wordlist.append(Word(user, "", word, "",  0, 0))
@@ -90,46 +92,48 @@ def add_word(wordlist,user,word):
 # Create and config logger
 LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig(filename= "/Users/jakob/documents/Python_Projects/diktat/diktat.log",
-                    level= logging.DEBUG,
-                    format= LOG_FORMAT,
+                    level=logging.DEBUG,
+                    format=LOG_FORMAT,
                     filemode='w')
 logger = logging.getLogger()
 
-#Test the logger
-#logger.debug("Test Msg Debug")
-#logger.info("Test Msg Info")
-#logger.warning("Test Msg Warning")
-#logger.error("Test Msg Error")
-#logger.critical("Test Msg Critical")
+# Test the logger
+# logger.debug("Test Msg Debug")
+# logger.info("Test Msg Info")
+# logger.warning("Test Msg Warning")
+# logger.error("Test Msg Error")
+# logger.critical("Test Msg Critical")
 
-def get_practice_wordlist_current(user,numberwords_current, wordlist):
+
+def get_practice_wordlist_current(user, numberwords_current, wordlist):
     practice_wordlist_current = random.sample(wordlist, numberwords_current)
     return practice_wordlist_current
 
-def get_practice_wordlist_previous(user,numberwords_previous,wordlist):
+
+def get_practice_wordlist_previous(user, numberwords_previous, wordlist):
     pass
 
-def select_practice_words(user,numberwords_current, numberwords_previous, wordlist):
+
+def select_practice_words(user, numberwords_current, numberwords_previous, wordlist):
     logger.debug("### Entered select_practice_words ###")
     practice_wordlist = []
-    practice_wordlist = get_practice_wordlist_current(user,numberwords_current, wordlist) #+ get_practice_wordlist_previous(user,numberwords_previous,wordlist)
+    practice_wordlist = get_practice_wordlist_current(user, numberwords_current, wordlist)  # get_practice_wordlist_previous(user,numberwords_previous,wordlist)
     return practice_wordlist
 
 
 def main():
-    #wordlist = []
-    wordlist = read_wordlist() #read current word list
-    #print(wordlist[0].word) #print word
-    #wordlist[0].test_word(bool(1)) #this is how to record a test of a word. 0->mistake, 1-> correct
+    # wordlist = []
+    wordlist = read_wordlist()  # read current word list
+    # print(wordlist[0].word)  # print word
+    # wordlist[0].test_word(bool(1)) # this is how to record a test of a word. 0->mistake, 1-> correct
 
-    #wordlist = add_word(wordlist,"Kind1","Tür")  #this is how to add a new word
-    practice_wordlist = select_practice_words("a",5,2,wordlist) #compile random practice wordlist
-    for i in range(0,len(practice_wordlist)): #print current practice wordlist
+    # wordlist = add_word(wordlist,"Kind1","Tür")  # this is how to add a new word
+    practice_wordlist = select_practice_words("a", 5, 2, wordlist)  # compile random practice wordlist
+    for i in range(0, len(practice_wordlist)):  # print current practice wordlist
         print(practice_wordlist[i].word)
 
-
-    write_status = write_wordlist(wordlist) #update/write wordlist to file
-    #print ("write_status: " + str(write_status))
+    write_status = write_wordlist(wordlist)  # update/write wordlist to file
+    # print ("write_status: " + str(write_status))
 
 if __name__ == "__main__":
     main()
